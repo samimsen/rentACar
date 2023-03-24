8e6c152789fd7bc663085b79360a573a67539dbc
Samim Sen <samim.sen@hotmail.com>
2023-03-25 01:49:27 +0300

    first commit

---
+++ src/main/java/kodlama/io/rentACar/core/utilities/exceptions/BusinessException.java
@@ -0,0 +1,13 @@
+package kodlama.io.rentACar.core.utilities.exceptions;
+
+public class BusinessException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BusinessException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
@@ -0,0 +1,43 @@
+package kodlama.io.rentACar.entities.concretes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "cars")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Car {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "plate", unique = true)
+	private String plate;
+	
+	@Column(name = "daily_price")
+	private double dailyPrice;
+	
+	@Column(name = "model_year")
+	private int modelYear;
+	
+	@Column(name = "state")
+	private int state;//1 available - 2 rented - 3 maintenance
+	
+	@ManyToOne
+	@JoinColumn(name = "model_id")
+	private Model model;
+
+}
